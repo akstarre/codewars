c72c37e6f063bceff6cd233b95bfbf27b6618f52
Kyle Starrett <akstarre@gmail.com>
2023-05-11 17:03:18 -0600

    create remove-all-marked-elements-of-a-list.js
    And fix to previous codewars.

--- all-inclusive.js
+++ all-inclusive.js
@@ -22,7 +22,7 @@
 
 // SOLUTION
 function containAllRots(str, arr) {
-    for (var i = 0; i < str.length; i++) {
+    for (i = 0; i < str.length; i++) {
       if (arr.indexOf(str.slice(i) + str.slice(0, i)) === -1) {
         return false
       }
