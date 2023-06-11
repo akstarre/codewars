78339a735977635ac53aafd9faa3adbf399050ff
Kyle Starrett <akstarre@gmail.com>
2023-06-11 15:26:41 -0600

    Create binary-addition

---
+++ binary-addition.js
@@ -0,0 +1,19 @@
+// DIRECTIONS
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+// SOLUTION
+function addBinary(a,b) {
+    return (a + b).toString(2)
+}
+
+let ab = 5
+let cd = 9
+
+console.log(addBinary(ab, cd))
