0b3e8c46f6b6e7e22dbed8eb987e33e6c27c5b5a
Kyle Starrett <akstarre@gmail.com>
2023-06-30 09:29:18 -0600

    Created Sum-of-mins, fixed typo in search-JSON

---
+++ sum-of-mins.js
@@ -0,0 +1,30 @@
+// DIRECTIONS
+// Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+// For Example:
+
+// [ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+// , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+// , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+// ]
+// So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+// Note: You will always be given a non-empty list containing positive values.
+
+// ENJOY CODING :)
+
+//SOLUTION
+function sumOfMinimums(arr) {
+    let mins = arr.map(arrs => {
+      arrs = arrs.sort((a,b)=> a-b)
+      return arrs[0]
+    })
+    return mins.reduce((acc, num)=> acc += num)
+  }
+
+
+  let arr = [ [ 1, 2, 3, 4, 5 ]
+  , [ 5, 6, 7, 8, 9 ] 
+  , [ 20, 21, 34, 56, 100 ]
+]
+  console.log(sumOfMinimums(arr))
