adcaf1a5a32c6bf85b4c36e277e3547995c8ca6a
Kyle Starrett <akstarre@gmail.com>
2023-07-06 15:54:15 -0600

    Create how-many-are-smaller-than-me.js

---
+++ how-many-are-smaller-than-me.js
@@ -0,0 +1,23 @@
+// DESCRIPTION:
+// Write a function that given, an array arr, returns an array containing at each index i the amount of numbers that are smaller than arr[i] to the right.
+
+// For example:
+
+// * Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
+// * Input [1, 2, 0] => Output [1, 1, 0]
+// If you've completed this one and you feel like testing your performance tuning of this same kata, head over to the much tougher version How many are smaller than me II?
+
+// SOLUTION
+function smaller(nums) {
+    return nums.map((num, i) => {
+        let counter = 0
+        for (j=i; j <= nums.length; j++){
+            if (num > nums[j]){
+                counter++
+            }
+        }
+        return counter
+    })
+}
+
+console.log(smaller([1, 2, 0]))
