0a2b4d73f0e74d65f528c67f1ed49da4c807c38d
Kyle Starrett <akstarre@gmail.com>
2023-07-25 16:37:23 -0600

    Update transform-to-prime.js

--- transform-to-prime.js
+++ transform-to-prime.js
@@ -20,9 +20,23 @@
 // 3- minimumNumber ({50,39,49,6,17,28}) ==> return (2)
 
 // SOLUTION
-function minimumNumber(numbers){
-    let sum = numbers.reduce((acc, ele)=>acc + ele, 0)
-    return sum
-  }
+function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function minimumNumber(numbers) {
+    let total = numbers.reduce((acc, curr) => acc + curr, 0);
+    let counter = 0;
+    while (true) {
+        if (isPrime(total + counter)) {
+            return counter;
+        }
+        counter++;
+    }
+}
 
   console.log(minimumNumber([2, 12, 8, 4, 6]))
