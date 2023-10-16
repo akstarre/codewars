740e5b42c2bf11a1cb312c5a3251abfd23088236
Kyle Starrett <akstarre@gmail.com>
2023-10-16 12:08:10 -0400

    refactoring sum-of-array-averages challenge

--- sum-of-array-averages.js
+++ sum-of-array-averages.js
@@ -3,9 +3,7 @@ const arry = [[3, 4, 1, 3, 5, 1, 4], [21, 54, 33, 21, 77]]
 
 
 const sumAverage = (arry) => {
-    return Math.floor(arry.map((arry) => {
-       return arry.reduce((acc, num)=> acc += num) / arry.length
-    }).reduce((acc, ave) => acc += ave))
+    return Math.floor(arry.map((arry) => arry.reduce((acc, num)=> acc += num) / arry.length).reduce((acc, ave) => acc += ave))
 }
 
 
